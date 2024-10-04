e69e408fb7c7836503cf725a63d762ab94884638
joelm <joelmoreraapaza@gmail.com>
2024-10-04 20:32:44 +0100

    Main added

---
+++ src/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Joel", LocalDate.of(2004, 1, 20)));
+    }
+}
